97e3a9fc81597f3c5ccff95c1721747afb234110
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2023-06-22 14:36:01 +0200

    Add entries export to prerender hidden songs

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,14 +1,19 @@
-import { getSong } from '$lib/server/songService';
+import { getSong, getSongList } from '$lib/server/songService';
 import { error } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { EntryGenerator, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = ({ params }) => {
-	const { slug } = params;
-	const song = getSong(slug);
-	if (!song) {
-		throw error(404);
-	}
-	return { song };
+  const { slug } = params;
+  const song = getSong(slug);
+  if (!song) {
+    throw error(404);
+  }
+  return { song };
+};
+
+export const entries: EntryGenerator = () => {
+  const { songs } = getSongList();
+  return songs.map((song) => ({ slug: song.slug }));
 };
 
 export const prerender = true;
