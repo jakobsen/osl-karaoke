dab7a26739a825c929f805b6ffb19dfa99b1c494
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2024-04-13 11:26:14 +0200

    Fix meta img

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -10,7 +10,10 @@
   <meta property="og:url" content="https://osloschlagerlag.no/{data.slug}" />
   <meta property="og:type" content="website" />
   <meta property="og:description" content="Live-karaoke med Oslo Schalgerlag på Salt" />
-  <meta property="og:image" content="https://osloschlagerlag.no/vokalisten-er-syk-20240413.jpeg" />
+  <meta
+    property="og:image"
+    content="https://osloschlagerlag.no/img/vokalisten-er-syk-20240413.jpeg"
+  />
   <meta property="og:image:type" content="image/jpeg" />
   <meta property="og:image:width" content="2000" />
   <meta property="og:image:height" content="1050" />
